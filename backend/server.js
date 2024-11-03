c044bcee6e17039272c5eb1399197c8e00b4c4f3
marshall nickolauson <marshnickol101@gmail.com>
2024-11-03 17:30:02 -0500

    setup frontend and backend

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { errorHandler } from './middleware/errorMiddleware.js';
+import { connectDB } from './config/db.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+const port = process.env.BACKEND_PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(cors());
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// app.use('/route', importedRoute)
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+import NotFoundPage from './pages/NotFoundPage';
+import HomePage from './pages/HomePage';
+
+function App() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />} >
+        <Route index element={<HomePage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />;
+}
+
+export default App
