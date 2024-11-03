21973a5ae80ec43fc48be4df043b7e7645738ee2
marshall nickolauson <marshnickol101@gmail.com>
2024-11-03 17:54:00 -0500

    added custom bootstrap styles and logo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { StrictMode } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/styles/bootstrap.custom.css'
+import './assets/styles/index.css'
 import store from './store.js'
 import App from './App.jsx'
-import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
