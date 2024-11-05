b4f35de96be8e0a1ea04e242e2ccfbe1e1a993a9
marshall nickolauson <marshnickol101@gmail.com>
2024-11-05 07:44:37 -0500

    feature: cart quantity number in navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,29 +1,36 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Container, Badge } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+import { useSelector } from "react-redux";
 
 function Header() {
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <LinkContainer to='/'>
+          <LinkContainer to="/">
             <Navbar.Brand>
-              <img src={logo} alt='ProShop' />
+              <img src={logo} alt="ProShop" />
               ProShop
             </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <LinkContainer to='/cart'>
+              <LinkContainer to="/cart">
                 <Nav.Link>
-                  <FaShoppingCart />
-                  Cart
+                  <FaShoppingCart /> Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
+                  )}
                 </Nav.Link>
               </LinkContainer>
-              <LinkContainer to='/login'>
+              <LinkContainer to="/login">
                 <Nav.Link>
                   <FaUser />
                   Sign in
