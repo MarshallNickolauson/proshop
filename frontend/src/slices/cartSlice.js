bccc04b55854bc75093144fe45570913adea4ca8
marshall nickolauson <marshnickol101@gmail.com>
2024-11-05 06:40:36 -0500

    add: cart reducer

---
+++ frontend/src/slices/cartSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : {cartItems: []};
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        
+    }
+});
+
+export default cartSlice.reducer;
