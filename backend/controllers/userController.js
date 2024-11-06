08016275c6b7ad6a0c76833246792b94713ccc1d
marshall nickolauson <marshnickol101@gmail.com>
2024-11-06 16:30:04 -0500

    add: user profile endpoints

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,7 +46,7 @@ export const registerUser = expressAsyncHandler(async (req, res) => {
     const userExists = await User.findOne({ email });
     if (userExists) {
         res.status(400);
-        throw new Error('User already exists');
+        throw new Error("User already exists");
     }
 
     const user = await User.create({
@@ -65,7 +65,7 @@ export const registerUser = expressAsyncHandler(async (req, res) => {
         });
     } else {
         res.status(400);
-        throw new Error('Invalid user data');
+        throw new Error("Invalid user data");
     }
 });
 
@@ -85,14 +85,47 @@ export const logoutUser = expressAsyncHandler(async (req, res) => {
 // @route   GET /api/users/profile
 // @access  Private
 export const getUserProfile = expressAsyncHandler(async (req, res) => {
-    res.send("Get user profile");
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
 });
 
 // @desc    Update user profile
 // @route   PUT /api/users/profile
 // @access  Private
 export const updateUserProfile = expressAsyncHandler(async (req, res) => {
-    res.send("Update user profile");
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.status(200).json({
+            _id: updatedUser._id,
+            name: updatedUser.name,
+            email: updatedUser.email,
+            isAdmin: updatedUser.isAdmin,
+        });
+    } else {
+        res.status(404);
+        throw new Error("User not found");
+    }
 });
 
 // @desc    Get users
