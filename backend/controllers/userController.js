df509ea3da4804f31cb168b430f91ba6380c79f6
marshall nickolauson <marshnickol101@gmail.com>
2024-11-06 16:24:59 -0500

    feature: register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,97 +3,122 @@ import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 
 const generateToken = (res, userId) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "30d",
-  });
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+    });
 
-  res.cookie("jwt", token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
-  });
+    res.cookie("jwt", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+    });
 };
 
 // @desc    Auth user & get token
 // @route   POST /api/users/login
 // @access  Public
 export const authUser = expressAsyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-  if (user && (await user.matchPassword(password))) {
-    generateToken(res, user._id);
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-    });
-  } else {
-    res.status(401);
-    throw new Error("Invalid email or password");
-  }
+    if (user && (await user.matchPassword(password))) {
+        generateToken(res, user._id);
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(401);
+        throw new Error("Invalid email or password");
+    }
 });
 
 // @desc    Register user
 // @route   POST /api/users
 // @access  Public
 export const registerUser = expressAsyncHandler(async (req, res) => {
-  res.send("Register user");
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+    });
+
+    if (user) {
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
 });
 
 // @desc    Logout user & clear cookie
 // @route   POST /api/users/logout
 // @access  Private
 export const logoutUser = expressAsyncHandler(async (req, res) => {
-  res.cookie("jwt", "", {
-    httpOnly: true,
-    expires: new Date(0),
-  });
+    res.cookie("jwt", "", {
+        httpOnly: true,
+        expires: new Date(0),
+    });
 
-  res.status(200).json({ message: "Logged out successfully" });
+    res.status(200).json({ message: "Logged out successfully" });
 });
 
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
 export const getUserProfile = expressAsyncHandler(async (req, res) => {
-  res.send("Get user profile");
+    res.send("Get user profile");
 });
 
 // @desc    Update user profile
 // @route   PUT /api/users/profile
 // @access  Private
 export const updateUserProfile = expressAsyncHandler(async (req, res) => {
-  res.send("Update user profile");
+    res.send("Update user profile");
 });
 
 // @desc    Get users
 // @route   GET /api/users
 // @access  Private/Admin
 export const getUsers = expressAsyncHandler(async (req, res) => {
-  res.send("Get users (admin)");
+    res.send("Get users (admin)");
 });
 
 // @desc    Get user by id
 // @route   GET /api/users/:id
 // @access  Private/Admin
 export const getUserById = expressAsyncHandler(async (req, res) => {
-  res.send("Get user by id (admin)");
+    res.send("Get user by id (admin)");
 });
 
 // @desc    Update user by id
 // @route   PUT /api/users/:id
 // @access  Private/Admin
 export const updateUserById = expressAsyncHandler(async (req, res) => {
-  res.send("Update user by id (admin)");
+    res.send("Update user by id (admin)");
 });
 
 // @desc    Delete user
 // @route   DELETE /api/users/:id
 // @access  Private/Admin
 export const deleteUserById = expressAsyncHandler(async (req, res) => {
-  res.send("Delete user by id (admin)");
+    res.send("Delete user by id (admin)");
 });
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -15,6 +15,11 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 }
 
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) next();
+    this.password = await bcrypt.hash(this.password, await bcrypt.genSalt(10));
+});
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
