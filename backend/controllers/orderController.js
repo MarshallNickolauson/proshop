a36d23a07e7fef7236f219c7832f851cd8d847f9
marshall nickolauson <marshnickol101@gmail.com>
2024-11-07 18:32:24 -0500

    update: order controller methods

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,25 +1,65 @@
-import expressAsyncHandler from "express-async-handler";
+import expressAsyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
 
 // @desc    Create new order
 // @route   POST /api/orders
 // @access  Private
 export const addOrderItems = expressAsyncHandler(async (req, res) => {
-    res.send('add order items');
+    const {
+        orderItems,
+        shippingAddresss,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+    } = req.body;
+
+    if (orderItems && orderItems.length === 0) {
+        res.status(400);
+        throw new Error('No order items');
+    } else {
+        const order = new Order({
+            orderItems: orderItems.map((x) => ({
+                ...x,
+                product: x._id,
+                _id: undefined,
+            })),
+            user: req.user._id,
+            shippingAddresss,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice,
+        });
+
+        const createdOrder = await order.save();
+
+        res.status(201).json(createdOrder);
+    }
 });
 
 // @desc    Get logged in user orders
 // @route   GET /api/orders/myorders
 // @access  Private
 export const getMyOrders = expressAsyncHandler(async (req, res) => {
-    res.send('get my orders');
+    const orders = await Order.find({ user: req.user._id });
+    res.status(200).json(orders);
 });
 
 // @desc    Get order by ID
 // @route   GET /api/orders/:id
 // @access  Private
 export const getOrderById = expressAsyncHandler(async (req, res) => {
-    res.send('get order by id');
+    const order = await Order.find(req.params.id).populate('user', 'name email');
+    
+    if (order) {
+        res.status(200).json(order);
+    } else {
+        res.status(404);
+        throw new Error('Order not found');
+    }
 });
 
 // @desc    Update order to paid
