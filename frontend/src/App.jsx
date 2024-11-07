f79eeafcd8b7d978752e93efde54a3eee85770a0
marshall nickolauson <marshnickol101@gmail.com>
2024-11-07 07:19:16 -0500

    add: jsx private routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
-import MainLayout from './layouts/MainLayout'
+import {
+    Route,
+    createBrowserRouter,
+    createRoutesFromElements,
+    RouterProvider,
+} from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
 import NotFoundPage from './screens/NotFoundPage';
 import HomePage from './screens/HomePage';
 import ProductPage from './screens/ProductPage';
@@ -7,24 +12,26 @@ import CartPage from './screens/CartPage';
 import LoginPage from './screens/LoginPage';
 import RegisterPage from './screens/RegisterPage';
 import ShippingPage from './screens/ShippingPage';
+import PrivateRoutes from './components/PrivateRoutes';
 
 function App() {
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path='/' element={<MainLayout />}>
+                <Route path='*' element={<NotFoundPage />} />
+                <Route index element={<HomePage />} />
+                <Route path='/product/:id' element={<ProductPage />} />
+                <Route path='/cart' element={<CartPage />} />
+                <Route path='/login' element={<LoginPage />} />
+                <Route path='/register' element={<RegisterPage />} />
+                <Route path='' element={<PrivateRoutes />}>
+                    <Route path='/shipping' element={<ShippingPage />} />
+                </Route>
+            </Route>
+        )
+    );
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path='/' element={<MainLayout />} >
-        <Route path='*' element={<NotFoundPage />} />
-        <Route index element={<HomePage />} />
-        <Route path='/product/:id' element={<ProductPage />} />
-        <Route path='/cart' element={<CartPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='/shipping' element={<ShippingPage />} />
-      </Route>
-    )
-  );
-
-  return <RouterProvider router={router} />;
+    return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
